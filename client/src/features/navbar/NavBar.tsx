e49f13c5ea0c6a9aac74589e10061de09ca791b8
Varvara Groznykh <varenkoluchka@gmail.com>
2023-08-24 17:59:06 +0300

    components

---
+++ client/src/features/navbar/NavBar.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './styles/style.scss';
+import { NavLink, Outlet } from 'react-router-dom';
+
+function NavBar(): JSX.Element {
+  return (
+    <>
+      <nav className="nav__container">
+        <ul className="nav__ul">
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? 'active_link' : '')} to="/">
+              Личный кабинет
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? 'active_link' : '')} to="/games">
+              Игра
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? 'active_link' : '')} to="/users">
+              Очки
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? 'active_link' : '')} to="/dsfaasdf">
+              Регистрация
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? 'active_link' : '')} to="/asdfadsf">
+              Авторизация
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? 'active_link' : '')} to="/asdaf">
+              Выйти
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  );
+}
+
+export default NavBar;
